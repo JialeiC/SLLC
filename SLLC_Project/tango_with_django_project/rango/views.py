780a7138c4887e932c61e99c0220bd47a44682e4
JialeiC <jc3592@nau.edu>
2021-08-05 23:55:26 +0800

    upload new homepage and categories page

--- SLLC_Project/tango_with_django_project/rango/views.py
+++ SLLC_Project/tango_with_django_project/rango/views.py
@@ -11,11 +11,12 @@ from django.core.paginator import Paginator
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-
+    page_list: object = Page.objects.order_by('-views')[:5]
+    question_list: object = Question.objects.order_by('-views')[:5]
     context_dict = {}
 
     context_dict['categories'] = category_list
+    context_dict['questions'] = question_list
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
 
@@ -34,12 +35,15 @@ def show_category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
+        questions = Question.objects.filter(category=category)
 
         context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['questions'] = questions
     except Category.DoesNotExist:
         context_dict['pages'] = None
         context_dict['category'] = None
+        context_dict['questions'] = None
     
     return render(request, 'rango/category.html', context=context_dict)
 
